6cb02339a759a3149d4a7cb42a8df27fa38e4aca
TranssLtd <166401998+TranssLtd@users.noreply.github.com>
2025-01-28 21:18:01 +0300

    Add files via upload

---
+++ Transs Job Help/script.js
@@ -0,0 +1,9 @@
+// Toggle Menu Functionality
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+});
+
+
